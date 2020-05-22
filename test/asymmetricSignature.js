9e25f00e2673623d375c397e214a3b306f809a7b
Luu Tuan Nguyen <miketuannguyen@gmail.com>
2020-05-23 06:56:08 +0700

    fix test file

--- test/asymmetricSignature.js
+++ test/asymmetricSignature.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 const RSA_PRIVATE_KEY = process.env.RSA_PRIVATE_KEY.replace(/\\n/g, '\n')
 const RSA_PUBLIC_KEY = process.env.RSA_PUBLIC_KEY.replace(/\\n/g, '\n')
+const PARTNER_CODE_PGP = process.env.PARTNER_CODE_PGP
 
 const privateKey = new NodeRSA(RSA_PRIVATE_KEY)
 const publicKey = new NodeRSA(RSA_PUBLIC_KEY)
@@ -10,8 +11,8 @@ const publicKey = new NodeRSA(RSA_PUBLIC_KEY)
 //sender's code
 const body = {
   amount: '10000',
-  partnerCode: 'PARTNER_CODE_RSA',
-  createdAt: '2020-05-21T02:47:30.829Z',
+  partnerCode: PARTNER_CODE_PGP,
+  createdAt: new Date(),
   secureHash: 'fa5dc4b9937410389107246c29e80e1347512d5f65e9baaf1438e008dca4c71b'
 }
 
