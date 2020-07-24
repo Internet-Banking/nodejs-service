c8fe19d55c14dbe41b2cd7ab05b3ee33f16f9b24
HoangNghia <hoangnghia.binhthuan@gmail.com>
2020-07-24 21:41:13 +0700

    remove socket.io from project

--- src/index.js
+++ src/index.js
@@ -1,34 +1,10 @@
 import moment from 'moment'
 import http from 'http'
-import socketIo from 'socket.io'
 import app from './app'
 import {PORT} from './config'
 import {debug} from './utils'
 
 const server = http.createServer(app)
-const io = socketIo(server)
-
-let interval;
-
-io.on('connection', (socket) => {
-  // eslint-disable-next-line no-console
-  console.log('New client connected');
-  if (interval) {
-    clearInterval(interval);
-  }
-  interval = setInterval(() => getApiAndEmit(socket), 1000);
-  socket.on('disconnect', () => {
-    // eslint-disable-next-line no-console
-    console.log('Client disconnected');
-    clearInterval(interval);
-  });
-});
-
-const getApiAndEmit = socket => {
-  const response = new Date();
-  // Emitting a new message. Will be consumed by the client
-  socket.emit('serverSendTime', response);
-};
 
 const NAMESPACE = `APP-${moment.utc().toISOString()}`
 // eslint-disable-next-line max-len
